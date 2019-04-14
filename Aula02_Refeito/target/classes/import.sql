41461d343ed270c0c5e6e758d88ab861ab559c78
Daniel Moudatsos <36973855+DanielMoudatsos@users.noreply.github.com>
2019-04-14 04:11:37 -0300

    Aula 02 Refeito

---
+++ Aula02_Refeito/target/classes/import.sql
@@ -0,0 +1,8 @@
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (1, 'Domingo', 13, 21, 69, 'Chuvoso');
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (2, 'Segunda', 12, 25, 71, 'Ensolarado');
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (3, 'Terça', 12, 19, 42, 'Seco');
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (4, 'Quarta', 15, 20, 57, 'Humido');
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (5, 'Quinta', 26, 31, 44, 'Nublado');
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (6, 'Sexta', 15, 22, 83, 'Nublado');
+insert into periodo(id, dia_semana, temperatura_minima, temperatura_maxima, humidade, descricao) values (7, 'Sábado', 26, 29, 94, 'Nublado');
+insert into usuario (id, login, senha) values (1, 'aluno', 'aluno')
